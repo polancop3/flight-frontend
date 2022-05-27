8e44b9088754c5f6e9cd25595434085076afb220
Pedro Polanco <pedropolanco02@gmail.com>
2022-05-27 16:21:27 -0400

    signup component change handle added

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,35 +1,49 @@
-import React, { Component } from 'react'
-export default class SignUp extends Component {
-  render() {
+import React, { Component, useState } from 'react'
+export default function SignUp() {
+  const[firstName, setFirstname] = useState("")
+  const[lastName, setlastname] = useState("")
+  const[email, setEmail] = useState("")
+  const[password, setPassword] = useState("")
+
+  function handlefirstNameChange(e) {
+    setFirstname(e.target.value)
+  }
+
+  function handleLastNameChange(e) {
+    setlastname(e.target.value)
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value)
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value)
+  }
+  console.log(`fname ${firstName} lname ${lastName} email: ${email} password ${password}`)
+
     return (
       <form>
         <h3>Sign Up</h3>
         <div className="mb-3">
           <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
+          <input type="text" className="form-control" placeholder="First name" value={firstName} 
+           onChange={handlefirstNameChange}
           />
         </div>
         <div className="mb-3">
           <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
+          <input type="text" className="form-control" placeholder="Last name" value={lastName} onChange={handleLastNameChange} />
         </div>
         <div className="mb-3">
-          <label>Username</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Enter username"
+          <label>Email</label>
+          <input type="text" className="form-control" placeholder="Enter Email" value={email} onChange={handleEmailChange}
           />
         </div>
         <div className="mb-3">
           <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
+          <input type="password" className="form-control" placeholder="Enter password" value={password}
+          onChange={handlePasswordChange}
           />
         </div>
         <div className="d-grid">
@@ -42,5 +56,4 @@ export default class SignUp extends Component {
         </p>
       </form>
     )
-  }
 }   
